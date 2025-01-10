957546f1ebe19ebd5638f8281488039a3fa86377
Daniyal Khokhar <danikhokhar22@gmail.com>
2025-01-11 00:40:45 +0500

    [MOD] Blog SEO data

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,6 +2,7 @@ import { createClient } from 'contentful';
 import CtfBlogCard from '@src/components/features/ctf-components/ctf-blog-card/ctf-blog-card';
 import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material';
+import Head from 'next/head';
 
 const useStyles = makeStyles((theme: Theme) => ({
     blogsList: {
@@ -38,16 +39,63 @@ export async function getStaticProps() {
     };
 }
 
+const metaTags = {
+    title: 'Blog',
+    description: '',
+    image: {
+        url: '',
+    },
+    no_index: false,
+    no_follow: false,
+};
+
+const robots = [
+    metaTags.no_index === true ? 'noindex' : undefined,
+    metaTags.no_follow === true ? 'nofollow' : undefined,
+].filter((x): x is string => x !== undefined);
+
 export default function Blogs({ blogs }) {
     const classes = useStyles();
     console.log(blogs);
     return (
-        <div className={classes.innerContainer}>
-            <div className={classes.blogsList}>
-                {blogs.map(blog => (
-                    <CtfBlogCard key={blog.sys.id} blog={blog} />
-                ))}
+        <>
+            <Head>
+                {metaTags.title && (
+                    <>
+                        <title key="title">{metaTags.title}</title>
+                        <meta key="og:title" property="og:title" content={metaTags.title} />
+                    </>
+                )}
+                {metaTags.description && (
+                    <>
+                        <meta key="description" name="description" content={metaTags.description} />
+                        <meta
+                            key="og:description"
+                            property="og:description"
+                            content={metaTags.description}
+                        />
+                    </>
+                )}
+                {robots.length > 0 && (
+                    <meta key="robots" name="robots" content={robots.join(', ')} />
+                )}
+                {metaTags.image && (
+                    <meta
+                        key="og:image"
+                        property="og:image"
+                        content={`${metaTags.image.url}?w=1200&h=630&f=faces&fit=fill`}
+                    />
+                )}
+                <meta key="og:url" property="og:url" content="blog" />
+                <meta key="og:locale" property="og:locale" content="" />
+            </Head>
+            <div className={classes.innerContainer}>
+                <div className={classes.blogsList}>
+                    {blogs.map(blog => (
+                        <CtfBlogCard key={blog.sys.id} blog={blog} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material';
 import Image from 'next/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import clsx from 'clsx';
+import Head from 'next/head';
 
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID ?? '',
@@ -75,21 +75,66 @@ const useStyles = makeStyles((theme: Theme) => ({
 }));
 
 export default function Blog({ post }) {
-    const { title, content, featuredImage } = post.fields;
+    const { title, content, featuredImage, slug } = post.fields;
     const classes = useStyles();
-    console.log(post);
+    const metaTags = {
+        title: title,
+        description: '',
+        image: {
+            url: '',
+        },
+        no_index: false,
+        no_follow: false,
+    };
+
+    const robots = [
+        metaTags.no_index === true ? 'noindex' : undefined,
+        metaTags.no_follow === true ? 'nofollow' : undefined,
+    ].filter((x): x is string => x !== undefined);
     return (
-        <div className={classes.innerContainer}>
-            <div className={classes.banner}>
-                <Image
-                    src={'https:' + featuredImage.fields.file.url}
-                    width={featuredImage.fields.file.details.image.width}
-                    height={featuredImage.fields.file.details.image.height}
-                    className={classes.featuredImage}
-                />
-                <h1 className={classes.title}>{title}</h1>
+        <>
+            <Head>
+                {metaTags.title && (
+                    <>
+                        <title key="title">{metaTags.title}</title>
+                        <meta key="og:title" property="og:title" content={metaTags.title} />
+                    </>
+                )}
+                {metaTags.description && (
+                    <>
+                        <meta key="description" name="description" content={metaTags.description} />
+                        <meta
+                            key="og:description"
+                            property="og:description"
+                            content={metaTags.description}
+                        />
+                    </>
+                )}
+                {robots.length > 0 && (
+                    <meta key="robots" name="robots" content={robots.join(', ')} />
+                )}
+                {metaTags.image && (
+                    <meta
+                        key="og:image"
+                        property="og:image"
+                        content={`${metaTags.image.url}?w=1200&h=630&f=faces&fit=fill`}
+                    />
+                )}
+                <meta key="og:url" property="og:url" content={slug} />
+                <meta key="og:locale" property="og:locale" content="" />
+            </Head>
+            <div className={classes.innerContainer}>
+                <div className={classes.banner}>
+                    <Image
+                        src={'https:' + featuredImage.fields.file.url}
+                        width={featuredImage.fields.file.details.image.width}
+                        height={featuredImage.fields.file.details.image.height}
+                        className={classes.featuredImage}
+                    />
+                    <h1 className={classes.title}>{title}</h1>
+                </div>
+                <div className={classes.contentWrap}>{documentToReactComponents(content)}</div>
             </div>
-            <div className={classes.contentWrap}>{documentToReactComponents(content)}</div>
-        </div>
+        </>
     );
 }
